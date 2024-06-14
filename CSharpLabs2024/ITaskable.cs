8902a43eddc21e01be9944d9ee71961aa89c9f2b
Danii1D <114641769+Danii1D@users.noreply.github.com>
2024-06-14 16:15:32 +0300

    Add files via upload

---
+++ CSharpLabs2024/ITaskable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLab2024
+{
+    public interface ITaskable
+    {
+        public void Run();
+    }
+}
